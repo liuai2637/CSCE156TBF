0cbfee5db384db183cd5918692d868bc1a0f54d6
Bryce <Bryce@DESKTOP-5G4TK7O>
2020-02-12 00:43:01 -0600

    I changed a bit

--- JavaProject/src/PersonDemo.java
+++ JavaProject/src/PersonDemo.java
@@ -17,34 +17,21 @@ public class PersonDemo {
 			e.printStackTrace();
 		}
 		int n = Integer.parseInt(s.nextLine());
+		String str =  s.nextLine();
 		
+		System.out.println(n);
+		System.out.println(str);
 		ArrayList<Person> peopleArrayList = new ArrayList<>();
 		
 	    s.useDelimiter("\n");
 	    
-	    while(s.hasNext()){
-	        String acc = s.nextLine();
-	        String[] info = acc.split(";", 5);
-	        String[] name = info[2].split(",");
-	        String[] address = info[3].split(",", 4);
-	        //TODO: check for country
-	        Address a = new Address(address[0], address[1], address[2], address[3], null);
-	        List<String> emails = Arrays.asList(info[4].split(","));
-	        // check if is broker
-	        if(!info[1].isEmpty()) {
-	        	Person p = new Person(info[0], info[1].charAt(0), info[1].substring(2), name[1], name[0], a, emails);
-	        	peopleArrayList.add(p);
-	        } else {
-	        	Person p = new Person(info[0], 'n', "n", name[1], name[0], a, emails);
-	        	peopleArrayList.add(p);
-	        }
-	        
-			/*
-			 * for(int i = 0; i < peopleArrayList.size(); i++) {
-			 * System.out.println(peopleArrayList.get(i).getPersonCode()); }
-			 */
-	    }    
+	    
+	    
+	    for(int i=1; i<=n; i++) {
+	    	
+	    }
 	    //TODO: output to xml
 	}
-
 }
+
+
